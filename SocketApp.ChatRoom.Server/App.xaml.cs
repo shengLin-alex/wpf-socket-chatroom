156da929365a7dd1a058fc7cd2bf8b60aea5d0aa
Jack Lin <a182820998@gmail.com>
2019-07-04 15:28:54 +0800

    Add global exception logger

--- SocketApp.ChatRoom.Server/App.xaml.cs
+++ SocketApp.ChatRoom.Server/App.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 using SocketApp.ChatRoom.Server.IOC;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace SocketApp.ChatRoom.Server
@@ -11,13 +13,22 @@ namespace SocketApp.ChatRoom.Server
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => this.LogUnhandledException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
+            this.DispatcherUnhandledException += (s, e) => this.LogUnhandledException(e.Exception, "Application.Current.DispatcherUnhandledException");
+            TaskScheduler.UnobservedTaskException += (s, e) => this.LogUnhandledException(e.Exception, "TaskScheduler.UnobservedTaskException");
+        }
+
+        private IContainer Container { get; set; }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             ContainerBuilder builder = new ContainerBuilder();
             AutofacBootstrapper.RegisterType(builder);
-            IContainer container = builder.Build();
+            this.Container = builder.Build();
 
-            using (ILifetimeScope lifeTimeScope = container.BeginLifetimeScope())
+            using (ILifetimeScope lifeTimeScope = this.Container.BeginLifetimeScope())
             {
                 ILoggerFactory loggerFactory = lifeTimeScope.Resolve<ILoggerFactory>();
                 loggerFactory.AddNLog(new NLogProviderOptions
@@ -28,6 +39,15 @@ namespace SocketApp.ChatRoom.Server
 
                 IMainWindow mainWindow = lifeTimeScope.Resolve<IMainWindow>();
                 mainWindow.ShowDialog();
+            }
+        }
+
+        private void LogUnhandledException(Exception exception, string eventName)
+        {
+            using (ILifetimeScope lifetimeScope = this.Container.BeginLifetimeScope())
+            {
+                ILogger<App> logger = lifetimeScope.Resolve<ILogger<App>>();
+                logger.LogError(exception, $"Error occur in {eventName}.");
             }
         }
     }
